907f761878a0b3af5e0e28f1ff167e676672c23b
githubbingry <fachry302405@gmail.com>
2024-04-18 02:12:45 +0700

    trying looping on base64

--- coba/base64.java
+++ coba/base64.java
@@ -1,18 +1,25 @@
-package GDSC_UNSRI;
+package coba;
 
 import java.util.Base64;
 import java.util.Scanner;
 
-public class EncodeDecodeBase64 {
+public class base64 {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         System.out.print("pilih 1 untuk encode base64 atau 2 untuk decode base64 : ");
         int opsi = x.nextInt();
+        System.out.print("berapa kali perulangan ? ");
+        int loop = x.nextInt();
+        x.nextLine();
         switch (opsi) {
             case 1:
             System.out.println("input string untuk di encode :");
             String test = x.nextLine();
             String encodedString = Base64.getEncoder().encodeToString(test.getBytes());
+            for(int i = 1; i < loop; i++){
+                test = encodedString;
+                encodedString = Base64.getEncoder().encodeToString(test.getBytes());
+            }
             System.out.println(encodedString);
                 break;
 
@@ -21,6 +28,11 @@ public class EncodeDecodeBase64 {
             test = x.nextLine();
             byte[] decodedBytes = Base64.getDecoder().decode(test);
             String decodedString = new String(decodedBytes);
+            for(int i = 1; i < loop; i++){
+                test = decodedString;
+                decodedBytes = Base64.getDecoder().decode(test);
+                decodedString = new String(decodedBytes);
+            }
             System.out.println(decodedString);
             default:
                 break;
