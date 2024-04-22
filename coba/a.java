b47da1010f23fbb08fb955143cc1ebac68cf01d6
githubbingry <fachry302405@gmail.com>
2024-04-22 22:27:38 +0700

    testing multiple assigning value to a variable

--- coba/a.java
+++ coba/a.java
@@ -6,6 +6,7 @@ public class a {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
         int n = x.nextInt(), m = x.nextInt(), mod = (int)4e5 + 69;
+
         // for (int i = n; i <= m * n; i += m){
         //     System.out.print((i ^ n ^ m) % mod + " ");
         // }
@@ -15,6 +16,11 @@ public class a {
         for (int i = n; i <= m * n--; i += m){
             System.out.print((i + n - m--) % mod + " ");
         }
+
+        // int t = n = m = 10;
+        // System.out.println(t);
+        // System.out.println(n);
+        // System.out.println(m);
         x.close();
     }
 }
